72ec59c36e836f809c05962fbd915237e00565f8
Stefans23 <stefans823@gmail.com>
2024-07-03 00:52:41 +0800

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ACTIONS_CORS_HEADERS } from '@solana/actions';
-import * as cors from 'cors';
+import cors from 'cors';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-import * as serverless from 'serverless-http';
+import express from 'express';
+import serverless from 'serverless-http';
 
 const expressApp = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(expressApp),
-  );
-  app.use(cors(ACTIONS_CORS_HEADERS));
-  await app.init(); // 初始化应用程序，但不监听端口
+  console.log('Starting Nest application...');
+  try {
+    const app = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(expressApp),
+    );
+    app.use(cors(ACTIONS_CORS_HEADERS));
+    await app.init(); // 初始化应用程序，但不监听端口
+    console.log('Nest application initialized');
+  } catch (error) {
+    console.error('Error during Nest application initialization:', error);
+  }
 }
 bootstrap();
 
